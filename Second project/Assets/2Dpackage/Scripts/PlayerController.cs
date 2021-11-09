0a274268e5919734cfaddf42ec37420252423f2b
momousa420 <mimity34@gmail.com>
2021-11-09 15:37:23 +0800

    211109
    碰撞體、重力控制

--- Second project/Assets/2Dpackage/Scripts/PlayerController.cs
+++ Second project/Assets/2Dpackage/Scripts/PlayerController.cs
@@ -7,11 +7,12 @@ public class PlayerController : MonoBehaviour
 
 
     public float moveSpeed;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,7 @@ public class PlayerController : MonoBehaviour
         RubyMove = transform.position;
         RubyMove.x = RubyMove.x + Input.GetAxis("Horizontal") * moveSpeed;
         RubyMove.y = RubyMove.y + Input.GetAxis("Vertical") * moveSpeed;
-        transform.position = RubyMove;
+        //transform.position = RubyMove;
+        rb.MovePosition(RubyMove);
     }
 }
